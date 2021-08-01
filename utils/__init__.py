a1516f55e1a0f0444f3e52152595266794f55891
JamesZhang22 <jameszhangjunjie@gmail.com>
2021-08-01 16:59:08 -0400

    add color selector

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,10 @@
 from .settings import *
-from .button import Button
+from .button import Buttons
+from .color_selector import *
 import pygame
+# from tkinter import *
+# from tkinter import colorchooser
+# from typing import Tuple
 
 pygame.init()
 pygame.font.init()
