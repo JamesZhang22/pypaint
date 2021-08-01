0fd81e70a052c281767eb3489b59d829ea41fdff
JamesZhang22 <jameszhangjunjie@gmail.com>
2021-08-01 16:59:45 -0400

    update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("PyPaint")
 pygame.mouse.set_cursor(*pygame.cursors.diamond)
 
-def init_grid(rows: int, cols: int, color: Tuple) -> List:
+def get_grid(rows: int, cols: int, color: Tuple) -> List:
     grid = []
 
     for row in range(rows):
@@ -29,9 +29,16 @@ def draw_grid(screen: pygame.Surface, grid: List) -> None:
             pygame.draw.line(screen, BLACK, (j * PIXEL_SIZE, 0), (j * PIXEL_SIZE, HEIGHT - TOOLBAR_HEIGHT))
 
 
-def draw(screen: pygame.Surface, grid: List, buttons: List) -> None:
+def draw_main(screen: pygame.Surface, grid: List, buttons: List) -> None:
     screen.fill(BG_COLOR)
     draw_grid(screen, grid)
+    for button in buttons:
+        button.draw(screen)
+    pygame.display.update()
+
+
+def draw_settings(screen: pygame.Surface, buttons: List) -> None:
+    screen.fill(BG_COLOR)
     for button in buttons:
         button.draw(screen)
     pygame.display.update()
@@ -50,11 +57,12 @@ def get_row_col_from_pos(pos: Tuple) -> Tuple:
 
 running = True
 clock = pygame.time.Clock()
-grid = init_grid(ROWS, COLS, BG_COLOR)
+grid = get_grid(ROWS, COLS, BG_COLOR)
 drawing_color = BLACK
 
 button_y = HEIGHT - TOOLBAR_HEIGHT/2 - 25
-buttons = [Button(10, button_y, 50, 50, BLACK), Button(70, button_y, 50, 50, RED), Button(130, button_y, 50, 50, GREEN), Button(190, button_y, 50, 50, BLUE), Button(250, button_y, 50, 50, WHITE, "Eraser", BLACK), Button(310, button_y, 50, 50, WHITE, "Clear", BLACK)]
+buttons = [Buttons(15, button_y, 50, 50, BLACK), Buttons(75, button_y, 50, 50, RED), Buttons(135, button_y, 50, 50, GREEN), Buttons(195, button_y, 50, 50, BLUE), Buttons(255, button_y, 50, 50, WHITE, None, BLACK, "images\paint-palette.png"), Buttons(315, button_y, 50, 50, WHITE, None, BLACK, "images\eraser-tool.png"), Buttons(375, button_y, 50, 50, WHITE, None, BLACK, "images\paint-bucket.png"), Buttons(435, button_y, 50, 50, WHITE, None, BLACK, "images\setting.png"), Buttons(50, button_y - 15, 400, 5, BLACK, None, BLACK)]
+settings_buttons = []
 
 while running:
     clock.tick(FPS)
@@ -74,12 +82,27 @@ while running:
                     if not button.clicked(pos):
                         continue
                     
-                    drawing_color = button.color
-                    if button.text == "Clear":
-                        grid = init_grid(ROWS, COLS, BG_COLOR)
+                    if button.x == 15:
                         drawing_color = BLACK
+                    elif button.x == 75:
+                        drawing_color = RED
+                    elif button.x == 135:
+                        drawing_color = GREEN
+                    elif button.x == 195:
+                        drawing_color = BLUE
+                    elif button.x == 255:
+                        drawing_color = choose_color()
+                    elif button.x == 315:
+                        drawing_color = WHITE
+                    elif button.x == 375:
+                        grid = get_grid(ROWS, COLS, drawing_color)
+                    elif button.x == 435:
+                        print(1) # holder event
+                    buttons[8].color = buttons[8].text_color = drawing_color
+                    
 
-    draw(SCREEN, grid, buttons)
+
+    draw_main(SCREEN, grid, buttons)
 
 
 pygame.quit()
