b88862fb6e2a3e1529666678d18db4efea62d158
JamesZhang22 <jameszhangjunjie@gmail.com>
2021-08-02 13:29:54 -0400

    new functions

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,8 @@
 from .settings import *
 from .button import Buttons
 from .color_selector import *
+from .save import *
+from .load import *
 import pygame
 # from tkinter import *
 # from tkinter import colorchooser
